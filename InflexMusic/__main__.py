fdcece718994edbca749665c4bd39aa3f59b3c10
xSanalOwner <legendmuzkao99@gmail.com>
2024-12-14 15:37:51 +0400

    Update __main__.py

--- InflexMusic/__main__.py
+++ InflexMusic/__main__.py
@@ -40,7 +40,7 @@ async def init():
     await userbot.start()
     await Inflex.start()
     try:
-        await Inflex.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Inflex.stream_call("https://telegra.ph/file/e91e886dd50ffdae88c99.jpg")
     except NoActiveGroupCall:
         LOGGER("InflexMusic").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
